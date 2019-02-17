aae325d6ce693d817d00708060e4519c4542bab3
IINamelessII <justnitro26@gmail.com>
2019-02-17 19:13:42 +0200

    Add stopwatches to 2nd lab

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -29,11 +29,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool wasGenerated = false;
   bool wasSorted = false;
-  static final lengths = new List<int>.generate(10, (i) => i * 2 + 1);
+  static final lengths = new List<int>.generate(10, (i) => i * 2 + 10);
   static final size = 10;
   var grid;
   String gridStr = '';
   String sortedGridStr = '';
+  String timesStr = '';
+  List<int> times = new List(size);
 
   void generate() {
     setState(() {
@@ -49,11 +51,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void sort() {
     setState(() {
-      grid = List<List<int>>.generate(size, (i) => sortHoare(grid[i]));
+      grid = List<List<int>>.generate(size, (i) => sortHoare(grid[i], i));
       sortedGridStr = grid.toString().replaceAll('], [', ']\n[');
       sortedGridStr = sortedGridStr.substring(1, sortedGridStr.length - 1);
       wasSorted = true;
+      timesStr = times.toString().replaceAll(', ', 'μs\nTime is: ');
+      timesStr = 'Time is: ' + timesStr.substring(1, timesStr.length - 1) + 'μs';
     });
+  }
+
+  List<int> sortHoare(List<int> list, int index) {
+    Stopwatch _watch = new Stopwatch()..start();
+    hoareSort(list, 0, list.length - 1);
+    times[index] = _watch.elapsedMicroseconds;
+//    print(index);
+//    times[index] = 3;
+    return list;
+  }
+
+  void hoareSort(List<int> array, int left, int right) {
+    int base, opposite, p, c;
+    base = left;
+    opposite = right;
+    while (base != opposite) {
+      if ((array[base] > array[opposite]) ^ (base > opposite)) {
+        c = array[base];
+        array[base] = array[opposite];
+        array[opposite] = c;
+        p = base;
+        base = opposite;
+        if (p < opposite) {
+          opposite = p + 1;
+        } else {
+          opposite = p - 1;
+        }
+      } else {
+        if (base < opposite) {
+          opposite--;
+        } else {
+          opposite++;
+        }
+      }
+    }
+    if (left < base - 1) hoareSort(array, left, base - 1);
+    if (base + 1 < right) hoareSort(array, base + 1, right);
   }
 
   @override
@@ -73,41 +114,9 @@ class _MyHomePageState extends State<MyHomePage> {
             (wasGenerated && !wasSorted
                 ? RaisedButton(onPressed: sort, child: Text('Sort'))
                 : Text('')),
-            Text(sortedGridStr),
+            Text(sortedGridStr + '\n'),
+            (wasSorted && wasGenerated ? Text(timesStr) : Text('')),
           ],
         ));
   }
-}
-
-List<int> sortHoare(List<int> list) {
-  hoareSort(list, 0, list.length - 1);
-  return list;
-}
-
-void hoareSort(List<int> array, int left, int right) {
-  int base, opposite, p, c;
-  base = left;
-  opposite = right;
-  while (base != opposite) {
-    if ((array[base] > array[opposite]) ^ (base > opposite)) {
-      c = array[base];
-      array[base] = array[opposite];
-      array[opposite] = c;
-      p = base;
-      base = opposite;
-      if (p < opposite) {
-        opposite = p + 1;
-      } else {
-        opposite = p - 1;
-      }
-    } else {
-      if (base < opposite) {
-        opposite--;
-      } else {
-        opposite++;
-      }
-    }
-  }
-  if (left < base - 1) hoareSort(array, left, base - 1);
-  if (base + 1 < right) hoareSort(array, base + 1, right);
 }
